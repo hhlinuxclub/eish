c1409139fcf41cdc7a373ae9730a71a7f357e47f
Joao Carlos <mail@joao-carlos.com>
2008-12-30 22:24:06 +0200

    Removed unneeded xml and comments.

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,6 +1,4 @@
 class Admin::NewsController < ApplicationController
-  # GET /news
-  # GET /news.xml
   def index
     @news = News.find(:all)
 
@@ -10,8 +8,6 @@ class Admin::NewsController < ApplicationController
     end
   end
 
-  # GET /news/1
-  # GET /news/1.xml
   def show
     @news_article = News.find(params[:id])
 
@@ -21,24 +17,18 @@ class Admin::NewsController < ApplicationController
     end
   end
 
-  # GET /news/new
-  # GET /news/new.xml
   def new
     @news_article = News.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @news_article }
     end
   end
 
-  # GET /news/1/edit
   def edit
     @news_article = News.find(params[:id])
   end
 
-  # POST /news
-  # POST /news.xml
   def create
     @news_article = News.new(params[:news])
 
@@ -46,16 +36,12 @@ class Admin::NewsController < ApplicationController
       if @news_article.save
         flash[:notice] = 'News was successfully created.'
         format.html { redirect_to(admin_news_article_path(@news_article)) }
-        format.xml  { render :xml => @news_article, :status => :created, :location => @news }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @news_article.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /news/1
-  # PUT /news/1.xml
   def update
     @news_article = News.find(params[:id])
 
@@ -63,23 +49,18 @@ class Admin::NewsController < ApplicationController
       if @news_article.update_attributes(params[:news_article])
         flash[:notice] = 'News was successfully updated.'
         format.html { redirect_to(admin_news_article_path(@news_article)) }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @news_article.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /news/1
-  # DELETE /news/1.xml
   def destroy
     @news_article = News.find(params[:id])
     @news_article.destroy
 
     respond_to do |format|
       format.html { redirect_to(admin_news_url) }
-      format.xml  { head :ok }
     end
   end
   
