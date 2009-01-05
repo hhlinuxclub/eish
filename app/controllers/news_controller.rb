f0e1fe55f5df1ebc10d121777c3e3800d181f716
Joao Carlos <mail@joao-carlos.com>
2009-01-05 02:42:19 +0200

    Index action now retrieves only published news.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ class NewsController < ApplicationController
   # GET /news
   # GET /news.xml
   def index
-    @news = News.find(:all).reverse
+    @news = News.find_all_by_published(true).reverse
 
     respond_to do |format|
       format.html # index.html.erb
