83b28f0ddd0bb37cb3420e972b95b6c77e44d0c6
Joao Carlos <mail@joao-carlos.com>
2009-01-09 22:55:08 +0200

    Improved format_event_dates method.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,13 +1,23 @@
 module EventsHelper
   def format_event_dates(event)
-    if (event.starts_at.to_date == event.ends_at.to_date)
-      if (event.starts_at.hour == 0 && event.starts_at.min == 0 && event.ends_at.hour == 23 && event.ends_at.min == 59)
+    # If all day
+    if event.starts_at.hour == 0 && event.starts_at.min == 0 && event.ends_at.hour == 23 && event.ends_at.min == 59
+      # If one day
+      if event.starts_at.to_date == event.ends_at.to_date
         return event.starts_at.strftime("%d %B %Y")
+      # If several days
       else
-        return event.starts_at.strftime("%d %B %Y, %H:%M") + " – " + event.ends_at.strftime("%H:%M")
+        return event.starts_at.strftime("%d %B %Y") + " – " + event.ends_at.strftime("%d %B %Y")
       end
+    # If not all day
     else
-       return event.starts_at.strftime("%d %B %Y, %H:%M") + " – " + event.ends_at.strftime("%d %B %Y, %H:%M")
+      # If one day
+      if event.starts_at.to_date == event.ends_at.to_date
+        return event.starts_at.strftime("%d %B %Y, %H:%M") + " – " + event.ends_at.strftime("%H:%M")
+      # If several days
+      else
+        return event.starts_at.strftime("%d %B %Y, %H:%M") + " – " + event.ends_at.strftime("%d %B %Y, %H:%M")
+      end
     end
   end
 end
