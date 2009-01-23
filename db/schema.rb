66c48b85dc62b8e577e07e60a4a6990d82591c56
Joao Carlos <mail@joao-carlos.com>
2009-01-23 08:46:55 +0800

    Added some initial data.
    Signed-off-by: Joao Carlos <mail@joao-carlos.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090109164604) do
+ActiveRecord::Schema.define(:version => 20090123001044) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
