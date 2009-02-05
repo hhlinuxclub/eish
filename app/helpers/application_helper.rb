1fd9f3e098ab34a698464725ebd62879421087ad
Joao Carlos <mail@joao-carlos.com>
2009-02-05 19:55:00 +0200

    Changed #navigation to not use #send.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,7 +36,7 @@ module ApplicationHelper
       else
         xhtml << "<li>"
       end
-      xhtml << link_to(c.capitalize, send(c.downcase + "_path"))
+      xhtml << link_to(c.capitalize, :controller => c)
       xhtml << "</li>\n"
     end
     
