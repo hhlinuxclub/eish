678c0701cf6651c71154ff369884d5fe49f19b35
Joao Carlos <mail@joao-carlos.com>
2009-02-11 01:07:06 +0200

    Added #link_to_profile. [#22 state:open]

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def link_to_profile(user)
+    name = user.first_name + " " + user.last_name
+    if user.username.nil?
+      return h(name)
+    else
+      return link_to name, "/profile/#{user.username}"
+    end
+  end
 end
