fce8048e4243457c8aa7470660e5eca8c94fe565
Joao Carlos <mail@joao-carlos.com>
2009-02-13 01:39:11 +0200

    Skip authorization for HomeController.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_filter :authorize
+  
   def index
     @news = News.find_all_by_published(true, :limit => 4, :order => "created_at DESC")
   end
