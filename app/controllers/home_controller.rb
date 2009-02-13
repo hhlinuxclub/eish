b20987e3a61eb737bc92bf77e424b78d3f2a0599
Joao Carlos <mail@joao-carlos.com>
2009-02-13 12:21:16 +0200

    Added more boxes to the home page. [#8]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   
   def index
     @news = News.find_all_by_published(true, :limit => 4, :order => "created_at DESC")
+    @events = Event.upcoming(4)
   end
 end
