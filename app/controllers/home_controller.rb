8fbe46fadc1770f674bb25a51bcc981bb6b712ad
Joao Carlos <mail@joao-carlos.com>
2009-02-23 10:28:55 +0200

    Show different information for a logged in user. [#8]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   def index
     @news = News.find_all_by_published(true, :limit => 4, :order => "created_at DESC")
     @events = Event.upcoming(4)
+    @user = User.find(session[:user_id]) unless session[:user_id].nil?
   end
 end
