9504c681efec8bfc24cc111df50e2c26cf2f256c
Joao Carlos <mail@joao-carlos.com>
2009-03-04 00:06:57 +0200

    Implemented the About page.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,3 +1,7 @@
 class AboutController < ApplicationController
   skip_before_filter :authorize
+  
+  def index
+    @about = RedCloth.new(Setting.option("about")).to_html
+  end
 end
---
+++ app/helpers/admin/about_helper.rb
@@ -0,0 +1,2 @@
+module Admin::AboutHelper
+end
