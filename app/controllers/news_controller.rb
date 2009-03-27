36602d412efa5498931506e55070e71b3014f774
Joao Carlos <mail@joao-carlos.com>
2009-03-27 22:27:55 +0200

    Deleted old comments.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,25 +1,21 @@
 class NewsController < ApplicationController
   skip_before_filter :authorize
   
-  # GET /news
-  # GET /news.xml
   def index
     @news = News.paginate_all_by_published(true, :include => :user, :page => params[:page], :per_page => 5, :order => "created_at DESC")
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.xml  { render :xml => @news }
       format.atom
     end
   end
 
-  # GET /news/1
-  # GET /news/1.xml
   def show
     @news_article = News.find_by_id_and_published(params[:id], true)
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.xml  { render :xml => @news_article }
     end
   end
