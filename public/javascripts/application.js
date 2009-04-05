44d709d47306d8304d214d51670ebf69a4f1da09
Joao Carlos <mail@joao-carlos.com>
2009-04-05 16:31:28 +0300

    Moved javascript into application.js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,43 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
+
+jQuery.fn.submitWithAjax = function() {
+  this.submit(function() {
+    $.post(this.action, $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+$(document).ready(function() {
+	$("#new_category").submitWithAjax();
+	
+	$("#bulk_check").click(function() {
+		this.checked = !(this.checked == true);
+		var ischecked = !(this.checked == true);
+		$("input:checkbox").attr("checked", function() {
+			this.checked = ischecked;
+		});
+	});
+	
+	$("#preview").dialog({
+		autoOpen: false,
+		bgiframe: true,
+		height: 400,
+		width: 700,
+		modal: true
+	});
+	
+	$('.date').datepicker({
+		changeMonth: true,
+		changeYear: true,
+		dateFormat: 'dd.mm.yy',
+		showButtonPanel: true,
+	});
+	
+	$('textarea.resizable:not(.processed)').TextAreaResizer();
+})
