d28805453951152a9ea3f422c2d018ffa92c0c3c
Joao Carlos <mail@joao-carlos.com>
2009-04-06 21:18:40 +0300

    Added categories to the public view of the website.  [#5]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,29 @@
 class ArticlesController < ApplicationController
   skip_before_filter :authorize
-  
-  # GET /articles
-  # GET /articles.xml
+
   def index
-    @articles = Article.all_published.reverse
+    @articles = Article.paginate_all_by_published(true, :page => params[:page], :per_page => 10, :limit => 5, :order => "created_at DESC")
+    @categories = Category.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.xml  { render :xml => @articles }
       format.atom { @articles = Article.find_all_by_published(true, :limit => 10, :order => "created_at DESC") } 
     end
   end
 
-  # GET /articles/1
-  # GET /articles/1.xml
   def show
     @article = Article.find_by_id_and_published(params[:id], true)
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.xml  { render :xml => @articles }
     end
+  end
+  
+  def categories
+    @category = Category.find(params[:id])
+    @articles = Article.paginate_all_by_published true, :page => params[:page], :per_page => 10, :joins => "INNER JOIN categorizations ON categorizations.article_id = articles.id INNER JOIN categories ON categories.id = categorizations.category_id", :conditions => ["categories.id = ?", params[:id]], :order => "articles.created_at DESC"
+    @categories = Category.all
   end
 end
