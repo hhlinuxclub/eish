6e1320cc3c1f44eafd39f7ee0185fe6a1a94770b
k33l0r <webadmin@draco-vulgaris.net>
2009-04-07 13:15:19 +0300

    Categories sorted by name (Thank you Joao)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.paginate_all_by_published(true, :page => params[:page], :per_page => 10, :limit => 5, :order => "created_at DESC")
-    @categories = Category.all
+    @categories = Category.all_alphabetically
 
     respond_to do |format|
       format.html
@@ -24,6 +24,6 @@ class ArticlesController < ApplicationController
   def categories
     @category = Category.find(params[:id])
     @articles = Article.paginate_all_by_published true, :page => params[:page], :per_page => 10, :joins => "INNER JOIN categorizations ON categorizations.article_id = articles.id INNER JOIN categories ON categories.id = categorizations.category_id", :conditions => ["categories.id = ?", params[:id]], :order => "articles.created_at DESC"
-    @categories = Category.all
+    @categories = Category.all_alphabetically
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :articles, :through => :categorizations
   
+  named_scope :all_alphabetically, :order => "name"
+  
   validates_uniqueness_of :name
 end
