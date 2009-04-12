73569b129c64570a3b9cc48267cb9eb30567a200
Joao Carlos <mail@joao-carlos.com>
2009-04-12 21:37:35 +0300

    Removed the controller admin/categories.

--- app/controllers/admin/categories_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::CategoriesController < ApplicationController
-  def create
-    @category = Category.create!(params[:category])
-    
-    respond_to do |format|
-      format.js
-    end
-  end
-end
