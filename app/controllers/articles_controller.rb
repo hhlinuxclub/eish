07421bcda4edce8117caa02c39a8415e48069483
Joao Carlos <mail@joao-carlos.com>
2009-04-15 00:02:23 +0300

    Smarter (shorter) way to get all published articles for a given category.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
   
   def categories
     @category = Category.find(params[:id])
-    @articles = Article.paginate_all_by_published true, :page => params[:page], :per_page => 10, :joins => "INNER JOIN categorizations ON categorizations.article_id = articles.id INNER JOIN categories ON categories.id = categorizations.category_id", :conditions => ["categories.id = ?", params[:id].to_i], :order => "articles.created_at DESC"
+    @articles = @category.articles.paginate_all_by_published true, :page => params[:page], :per_page => 10, :order => "articles.created_at DESC"
     @categories = Category.all_alphabetically
   end
 end
