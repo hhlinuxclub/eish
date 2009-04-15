df2c06b60b9ec1efe62b896df32c3707020ea4bd
k33l0r <webadmin@draco-vulgaris.net>
2009-04-15 20:59:52 +0300

    Added pre button [#21]

--- public/javascripts/markitup.textile.js
+++ public/javascripts/markitup.textile.js
@@ -32,6 +32,7 @@ mySettings = {
 		{name:'Link', openWith:'"', closeWith:'([![Title]!])":[![Link:!:http://]!]', placeHolder:'Your text to link here...' },
 		{separator:'---------------' },
 		{name:'Quotes', openWith:'bq(!(([![Class]!])!)). '},
-		{name:'Code', openWith:'@', closeWith:'@'}
+		{name:'Code', openWith:'@', closeWith:'@'},
+		{name:'Pre', openWith:'<pre>', closeWith:'</pre>'}
 	]
 };
