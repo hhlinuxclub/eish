1555da7afd4b05b52f6e1d7fc2750bce09aa2a5a
k33l0r <webadmin@draco-vulgaris.net>
2009-04-16 23:17:57 +0300

    Added colour picker

--- public/javascripts/markitup.textile.js
+++ public/javascripts/markitup.textile.js
@@ -57,6 +57,44 @@ mySettings = {
 					}
 				return textile;
 			}
+		},
+		{	name:'Colors', className:'palette', dropMenu: [
+				{name:'Yellow',	replaceWith:'#FCE94F',	className:"col1-1" },
+				{name:'Yellow',	replaceWith:'#EDD400', 	className:"col1-2" },
+				{name:'Yellow', replaceWith:'#C4A000', 	className:"col1-3" },
+				
+				{name:'Orange', replaceWith:'#FCAF3E', 	className:"col2-1" },
+				{name:'Orange', replaceWith:'#F57900', 	className:"col2-2" },
+				{name:'Orange', replaceWith:'#CE5C00', 	className:"col2-3" },
+				
+				{name:'Brown', 	replaceWith:'#E9B96E', 	className:"col3-1" },
+				{name:'Brown', 	replaceWith:'#C17D11', 	className:"col3-2" },
+				{name:'Brown',	replaceWith:'#8F5902', 	className:"col3-3" },
+				
+				{name:'Green', 	replaceWith:'#8AE234', 	className:"col4-1" },
+				{name:'Green', 	replaceWith:'#73D216', 	className:"col4-2" },
+				{name:'Green',	replaceWith:'#4E9A06', 	className:"col4-3" },
+				
+				{name:'Blue', 	replaceWith:'#729FCF', 	className:"col5-1" },
+				{name:'Blue', 	replaceWith:'#3465A4', 	className:"col5-2" },
+				{name:'Blue',	replaceWith:'#204A87', 	className:"col5-3" },
+	
+				{name:'Purple', replaceWith:'#AD7FA8', 	className:"col6-1" },
+				{name:'Purple', replaceWith:'#75507B', 	className:"col6-2" },
+				{name:'Purple',	replaceWith:'#5C3566', 	className:"col6-3" },
+				
+				{name:'Red', 	replaceWith:'#EF2929', 	className:"col7-1" },
+				{name:'Red', 	replaceWith:'#CC0000', 	className:"col7-2" },
+				{name:'Red',	replaceWith:'#A40000', 	className:"col7-3" },
+				
+				{name:'Gray', 	replaceWith:'#FFFFFF', 	className:"col8-1" },
+				{name:'Gray', 	replaceWith:'#D3D7CF', 	className:"col8-2" },
+				{name:'Gray',	replaceWith:'#BABDB6', 	className:"col8-3" },
+				
+				{name:'Gray', 	replaceWith:'#888A85', 	className:"col9-1" },
+				{name:'Gray', 	replaceWith:'#555753', 	className:"col9-2" },
+				{name:'Gray',	replaceWith:'#000000', 	className:"col9-3" }
+			]
 		}
 	]
 };
