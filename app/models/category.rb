2e28a247d8fb3d935591124b3a255bbc5425a316
Joao Carlos <mail@joao-carlos.com>
2009-04-17 19:39:01 +0300

    Disallow empty category names.

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,7 @@ class Category < ActiveRecord::Base
   named_scope :all_alphabetically, :order => "name"
   
   validates_uniqueness_of :name
+  validates_presence_of :name
   
   def to_param
     "#{id}-#{name.parameterize}"
