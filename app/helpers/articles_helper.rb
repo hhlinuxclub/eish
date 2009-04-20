857a20904f55cb1dd60219e66eb2a89dd348343d
k33l0r <webadmin@draco-vulgaris.net>
2009-04-20 12:58:08 +0300

    Fixed category cloud, remove category_table method. [#74 state:resolved]

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,60 +1,26 @@
 module ArticlesHelper
-  def cloudify(categories)
+  def cloudify(categories, min_font_size = 100, max_font_size = 140, span_css_style = "display:inline-block;")
     xhtml = ""
+    categories_array = Array.new
     
     categories.each do |c|
-      articles_count = c.articles.count
+      categories_array.push(c.articles.count)
+    end
+    
+    categories_array.sort!
+    
+    min_occurs = categories_array.at(0)
+    max_occurs = categories_array.last
+    
+    categories.each do |c|
+      weight = (Math.log(c.articles.count)-Math.log(min_occurs))/(Math.log(max_occurs)-Math.log(min_occurs))
+      font_size_of_current_category = (min_font_size + ((max_font_size-min_font_size)*weight)).round
       
-        if articles_count > 0
-          size = 0.7 * Math.log(articles_count)
-        else
-          size = 0.6 * Math.log(1)
-        end
-
-      xhtml << "#{link_to c.name, categories_path(c), :style => "font-size: " + size.to_s + "em"} (" + articles_count.to_s + ") &nbsp;"
+      xhtml << "<span style=\"" + span_css_style + "\">"
+      xhtml << "#{link_to c.name, categories_path(c), :style => "font-size: " + font_size_of_current_category.to_s + "%"} (" + c.articles.count.to_s + ") &nbsp;"
+      xhtml << "</span>"
     end
     
     return xhtml
-  end
-  
-  def category_table(categories, id = "category_table", columns = 5)
-    xhtml = "<table id=\""+ id + "\">"
-    count = 0;
-    rows = categories.count/columns 
-    total_count = 0;
-    
-    if categories.count%columns !=0 && categories.count > columns
-      rows += 1
-    end
-    
-    extra_cells = (rows * columns) - categories.count
-    
-    categories.each do |c|
-      count = count + 1
-      total_count = total_count + 1
-      
-      if count == 1
-        xhtml << "<tr>"
-      end
-      
-      xhtml << "<td>#{link_to c.name, categories_path(c)} (#{c.articles.count.to_s})</td>"
-      
-      if count == columns && total_count != categories.count
-        xhtml << "</tr>"
-        count = 0
-      end
-    end
-    
-    extra_cells.times do
-      xhtml << "<td>&nbsp;</td>"
-    end
-    
-    
-    xhtml << "</tr></table>"
-    return xhtml
-  end
-  
-  def new_cloudify(categories)
-    
   end
 end
