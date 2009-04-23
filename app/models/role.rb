4fbdef096a0fea7231ceb0340786f3f3358819e5
Joao Carlos <mail@joao-carlos.com>
2009-04-23 23:45:21 +0300

    Added #privileges to return an array containing descriptions of each privilege.

--- app/models/role.rb
+++ app/models/role.rb
@@ -9,4 +9,20 @@ class Role < ActiveRecord::Base
       end
     end
   end
+  
+  def privileges
+    privileges = []
+    
+    if can_create?
+      privileges << "create content"
+      privileges << "edit your own content" unless can_update?
+      privileges << "delete your own content" unless can_delete?
+    end
+    privileges << "edit any content" if can_update?
+    privileges << "delete any content" if can_delete?
+    privileges << "publish and unpublish content" if can_publish
+    privileges << "administer users and settings" if can_administer?
+    
+    return privileges
+  end
 end
