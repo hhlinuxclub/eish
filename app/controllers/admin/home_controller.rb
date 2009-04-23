58820fa4cc03438d5352495246489deb6a02ab63
Joao Carlos <mail@joao-carlos.com>
2009-04-23 23:46:51 +0300

    Started replacing the home page in the administration.

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,19 +3,6 @@ class Admin::HomeController < ApplicationController
   layout "admin"
  
   def index
-    @welcome_message = Setting.option("welcome_message")
-  end
-  
-  def update
-    setting = Setting.find_by_option("welcome_message")
-    
-    respond_to do |format|
-      if setting.update_attribute("value", params[:welcome_message])
-        flash[:notice] = "Welcome message successfully saved."
-      else
-        flash[:error] = "Something went wrong..."
-      end
-      format.html { redirect_to :controller => "home" }
-    end
+    @role = User.find(session[:user_id]).role
   end
 end
