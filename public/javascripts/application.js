1df0a019d04cf1f2860284c531d2f7dde8b09976
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-04-28 00:03:19 +0300

    Replaced help images with spans.  [#10]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -73,10 +73,12 @@ $(document).ready(function() {
 		messageloc: 1,
 		userid:	"#user_username"
 	});
+	
+	$('.clueTip').removeAttr("style");
   
-  $('.clueTip').cluetip({
-    splitTitle: '|', // use the invoking element's title attribute to populate the clueTip...
-                     // ...and split the contents into separate divs where there is a "|"
-    showTitle: false // hide the clueTip's heading
-  });
+	$('.clueTip').cluetip({
+	    splitTitle: '|', // use the invoking element's title attribute to populate the clueTip...
+	                     // ...and split the contents into separate divs where there is a "|"
+	    showTitle: false // hide the clueTip's heading
+	});
 })
