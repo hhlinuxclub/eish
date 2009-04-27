9ee6d395c2a045a9c15ce23c85b8b29102d456c9
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-04-27 23:38:45 +0300

    Tentative implementations of JS helpers for the registration form. [#10 state:open]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -73,4 +73,10 @@ $(document).ready(function() {
 		messageloc: 1,
 		userid:	"#user_username"
 	});
+  
+  $('.clueTip').cluetip({
+    splitTitle: '|', // use the invoking element's title attribute to populate the clueTip...
+                     // ...and split the contents into separate divs where there is a "|"
+    showTitle: false // hide the clueTip's heading
+  });
 })
