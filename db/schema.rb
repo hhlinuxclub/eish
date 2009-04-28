6621379fc1f954b4d97cadcc9527e4103fd163f5
Joao Carlos <mail@joao-carlos.com>
2009-04-29 01:02:04 +0300

    Added titles to users.  [#85 state:resolved]

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090414211323) do
+ActiveRecord::Schema.define(:version => 20090428213937) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(:version => 20090414211323) do
     t.boolean  "contactable",            :default => false
     t.string   "reset_hash"
     t.datetime "reset_hash_expires"
+    t.string   "title"
   end
 
 end
