fce48ca0b211d28d6cb1c2cce6147898660fa650
k33l0r <korhonen.matt@gmail.com>
2009-04-28 17:54:50 +0300

    Deleted some comments and extra whitespace

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -77,13 +77,12 @@ $(document).ready(function() {
 	$('.clueTip').removeAttr("style");
   
 	$('.clueTip').cluetip({
-	    splitTitle: '|', // use the invoking element's title attribute to populate the clueTip...
-	                     // ...and split the contents into separate divs where there is a "|"
-	    showTitle: false, // hide the clueTip's heading
+	    splitTitle: '|',
+	    showTitle: false,
 	    activation: 'click',
 		fx: {             
-                      open:       'slideDown', // can be 'show' or 'slideDown' or 'fadeIn'
-                      openSpeed:  'slow'
+        	open:       'slideDown',
+        	openSpeed:  'slow'
     	}
 	});
 })
