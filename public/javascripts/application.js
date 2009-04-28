1fb23612b0c8069d97624b60a717f913312e7477
k33l0r <korhonen.matt@gmail.com>
2009-04-28 17:44:35 +0300

    Enough JS helpers and validation, our users can't be that stupid [#10 milestone:"0.3 (Beta)" state:closed]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -79,6 +79,11 @@ $(document).ready(function() {
 	$('.clueTip').cluetip({
 	    splitTitle: '|', // use the invoking element's title attribute to populate the clueTip...
 	                     // ...and split the contents into separate divs where there is a "|"
-	    showTitle: false // hide the clueTip's heading
+	    showTitle: false, // hide the clueTip's heading
+	    activation: 'click',
+		fx: {             
+                      open:       'slideDown', // can be 'show' or 'slideDown' or 'fadeIn'
+                      openSpeed:  'slow'
+    	}
 	});
 })
