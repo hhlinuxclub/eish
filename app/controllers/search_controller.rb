77cec3cfd545a93306139431adc7a324a75c5d1d
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-05-02 14:40:40 +0300

    Added meta tags to the search pages. [#11 #73]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,9 @@ class SearchController < ApplicationController
   # TODO: Stop search from functioning if it has been disabled in the settings
   
   def index
+    set_meta_tags :title => "Search",
+                  :description => "Search the HHLinuxClub site",
+                  :keywords => "search"
   end
 
   def results
@@ -11,20 +14,36 @@ class SearchController < ApplicationController
     @articles = Article.search(@query, :per_page => 3)
     @news = News.search(@query, :per_page => 3)
     @events = Event.search(@query, :per_page => 3)
+    
+    set_meta_tags :title => "Searched for '" + @query + "'",
+                  :description => "Searched for '" + @query + "'",
+                  :keywords => "articles, events, news, search, " + @query
   end
 
   def articles
     @query = params[:query]
     @articles = Article.search(@query, :per_page => 10, :page => params[:page])
+    
+    set_meta_tags :title => "Searched for '" + @query + "'",
+                  :description => "Searched for '" + @query + "'",
+                  :keywords => "articles, search, " + @query
   end
   
   def news
     @query = params[:query]
     @news = News.search(@query, :per_page => 10, :page => params[:page])
+    
+    set_meta_tags :title => "Searched for '" + @query + "'",
+                  :description => "Searched for '" + @query + "'",
+                  :keywords => "news, search, " + @query
   end
   
   def events
     @query = params[:query]
     @events = Event.search(@query, :per_page => 10, :page => params[:page])
+    
+    set_meta_tags :title => "Searched for '" + @query + "'",
+                  :description => "Searched for '" + @query + "'",
+                  :keywords => "events, search, " + @query
   end 
 end
