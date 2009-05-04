10780d98e33a75b3958ec3f13cd69e808706170d
Joao Carlos <mail@joao-carlos.com>
2009-05-04 11:47:21 +0300

    Added validations to Gallery.  [#42]

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,6 +2,8 @@ class Gallery < ActiveRecord::Base
   belongs_to :user
   has_many :assets, :as => :attachable
   
+  validates_presence_of :name, :description
+  
   def to_param
     "#{id}-#{name.parameterize}"
   end
