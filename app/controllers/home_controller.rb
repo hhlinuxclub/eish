e4ba0ffd6d2520a2e856b92ef7bd5ef9ba2fd64e
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-05-09 15:16:06 +0300

    Added ongoing events to the homepage [#102 state:resolved]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
   
   def index
     @news = News.find_all_by_published(true, :limit => 4, :order => "created_at DESC")
-    @events = Event.upcoming(4)
+    
+    ongoing_events = Event.ongoing
+    upcoming_events = Event.upcoming(4 - ongoing_events.count)
+    @events = ongoing_events + upcoming_events
+    
     @user = User.find(session[:user_id]) unless session[:user_id].nil?
     @welcome_message = Setting.option("welcome_message")
     @featured_article = Article.featured
