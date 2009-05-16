b09b808aeed6673f0e48e9f46e78019ad1e75a44
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-05-16 19:06:02 +0300

    Added a before_filter to check the format of the email address, and the presence of the subject, name, and message. [#117 state:open]

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,5 +1,6 @@
 class ContactController < ApplicationController
   skip_before_filter :authorize
+  before_filter :check_form, :only => :send_email
   
   def index
     @contacts = User.find_all_by_contactable(true)
@@ -26,6 +27,29 @@ class ContactController < ApplicationController
         flash[:error] = "Captcha failed. Please try again."
       end
       format.html { redirect_to :controller => "contact" }
+    end
+  end
+  
+  protected
+  
+  def check_form
+    if session[:user_id].nil?
+      email_reg = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+      valid_email = email_reg.match(params[:email])? true : false
+      empty_name = params[:name].empty?
+    else
+      valid_email = true
+      empty_name = false
+    end
+    
+    empty_subject = params[:subject].empty?
+    empty_message =  params[:message].empty?
+    
+    if valid_email == false || empty_name || empty_subject || empty_message
+      flash[:error] = "Your message could not be sent."
+      respond_to do |format|
+        format.html { redirect_to :controller => "contact" }
+      end
     end
   end
 end
