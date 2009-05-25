6a62423aadea04e61de05c62312e879a9e689154
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-05-25 15:42:17 +0300

    Added support for categories, past events, and XHTML rendering [#76]

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -6,9 +6,12 @@ class SitemapController < ApplicationController
     @news = News.paginate_all_by_published(true, :include => :user, :page => params[:page], :per_page => 5, :order => "created_at DESC")
     @upcoming_events = Event.upcoming
     @ongoing_events = Event.ongoing
+    @past_events = Event.past
+    @categories = Category.all_alphabetically
     
     respond_to do |format|
       format.xml { render :layout => false }
+      format.html
     end
   end
 end
