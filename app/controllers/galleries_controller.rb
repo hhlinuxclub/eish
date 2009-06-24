bb27555e2f8de143f674b7014b434cbb605dcb8b
Joao Carlos <mail@joao-carlos.com>
2009-06-24 04:13:54 +0300

    Added a page to show a certain gallery [#42 state:testing]

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -9,5 +9,11 @@ class GalleriesController < ApplicationController
   end
 
   def show
+      @gallery = Gallery.find_by_id_and_published(params[:id].to_i, true)
+      @columns = 4 # TODO: Move this to the settings table
+      
+      respond_to do |format|
+          format.html
+      end
   end
 end
