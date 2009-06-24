27934feabc24c24c2f237785885b8cb3ff8496a6
Joao Carlos <mail@joao-carlos.com>
2009-06-24 04:10:17 +0300

    Added a helper to easily create tables with images

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,2 +1,15 @@
 module GalleriesHelper
+  def image_table(collection, columns)
+    concat("<table id=\"gallery\">\n")
+    collection.each_with_index do |element, index|
+      concat("<tr>\n") if index % columns == 0
+      concat("<td>\n<div class=\"image\">")
+      yield(element)
+      concat("</div>\n</td>\n")
+      if (index + 1) % columns == 0 || index == collection.count - 1
+        concat("</tr>")
+      end
+    end
+    concat("</table>")
+  end
 end
