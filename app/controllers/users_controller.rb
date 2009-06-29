9b8b481495f86eb72db4ac4b89e3d16a018eff5d
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-06-29 17:54:24 +0300

    A hacked up solution to redirect the user to the correct page after login/logout.  This still needs work (I'm not sure if this is the correct/safe way of implementing this...) [#105]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   before_filter :require_https, :only => [:login, :new, :request_credentials, :edit] if HTTPS_FOR_LOGINS
   before_filter :not_logged_in, :only => [:login, :new]
-  
+
   def login
     set_meta_tags :title => 'Login',
                   :description => 'Login page',
                   :keywords => 'Site, Login, Users'
-    
+
     if request.post?
       @user = User.authenticate(params[:username], params[:password])
       if @user
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
       else
         flash[:error] = "Invalid user/password combination."
       end
-        
+
       respond_to do |format|
-        format.html { redirect_to @user ? :root : :login }
+        format.html { redirect_to params[:request_uri] }
       end
     end
   end
@@ -30,42 +30,42 @@ class UsersController < ApplicationController
     User.find(session[:user_id]).forget_me
     session[:user_id] = nil
     flash[:notice] = "Logged out."
-    
+
     respond_to do |format|
-      format.html { redirect_to :root }
+      format.html { redirect_to request.referrer }
     end
   end
-  
+
   def show
     redirect_to :action => "edit"
   end
-  
+
   def new
     @user = User.new
-    
+
     set_meta_tags :title => "Register",
                   :description => "Register as a HHLinuxClub user",
                   :keywords => "Register, Users"
-    
+
     respond_to do |format|
       format.html
     end
   end
-  
+
   def edit
     @user = User.find_by_username(params[:id])
-    
+
     redirect_to :root and return unless @user.id == session[:user_id]
-    
+
     respond_to do |format|
       format.html
     end
   end
-  
+
   def create
     @user = User.new(params[:user])
     saved = RCC_ENABLED ? @user.valid? && validate_recap(params, @user.errors) && @user.save : @user.save
-    
+
     respond_to do |format|
       if saved
         session[:user_id] = @user.id
@@ -76,12 +76,12 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def update
     @user = User.find_by_username(params[:id])
-    
+
     redirect_to :root and return unless @user.id == session[:user_id]
-    
+
     respond_to do |format|
       if User.encrypted_password(params[:user][:current_password], @user.salt) == @user.hashed_password
         if @user.update_attributes(params[:user])
@@ -98,21 +98,21 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def remove
     @user = User.find_by_username(params[:id])
-    
+
     redirect_to :root and return unless @user.id == session[:user_id]
-    
+
     respond_to do |format|
       format.html
     end
   end
-  
+
   def destroy
     user = User.find(session[:user_id])
     current_hashed_password = User.encrypted_password(params[:user][:current_password], user.salt)
-    
+
     respond_to do |format|
       if current_hashed_password == user.hashed_password
         if user.safe_destroy
@@ -129,12 +129,12 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def request_credentials
     set_meta_tags :title => "Request credentials",
                   :description => "Request credentials",
                   :keywords => "Users"
-    
+
     respond_to do |format|
       if session[:user_id].nil?
         format.html
@@ -143,11 +143,11 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def send_credentials
     user = User.find_by_email(params[:email])
     user.generate_reset_hash
-    
+
     respond_to do |format|
       if user.nil?
         flash[:error] = "The email you entered is not associated with any user."
@@ -163,19 +163,19 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def password_reset
     @user = User.find_by_username(params[:username])
     @reset_hash = params[:reset_hash]
-    
+
     respond_to do |format|
       format.html
     end
   end
-  
+
   def update_password
     user = User.find_by_username(params[:username])
-    
+
     respond_to do |format|
       if params[:password] == params[:password_confirmation]
         if user.reset_password(params[:password], params[:reset_hash])
@@ -190,17 +190,17 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   protected
-  
+
   def require_https
     redirect_to :protocol => "https://" unless (request.ssl? or local_request?)
-  end 
-  
+  end
+
   def not_logged_in
     if !session[:user_id].nil?
         flash[:error] = "You are already logged in"
-        
+
         respond_to do |format|
           format.html { redirect_to :root }
         end
