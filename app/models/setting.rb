6d03958ff7527ecb46715bc44e272f24f4c22008
Joao Carlos <mail@joao-carlos.com>
2009-07-01 20:44:25 +0300

    Added Setting.featured_article_id

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,9 @@
 class Setting < ActiveRecord::Base
   def self.option(option)
-    return find_by_option(option).value
+    find_by_option(option).value
+  end
+  
+  def self.featured_article_id
+    option("featured_article").to_i
   end
 end
