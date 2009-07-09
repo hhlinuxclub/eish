ce52d4dc56a24d2c649b6b187114319dab6c4331
Joao Carlos <mail@joao-carlos.com>
2009-07-09 18:57:04 +0300

    Added the CodeRay gem. Bundled a file from CodeRay's repository to support the latest RedCloth version.

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,8 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'coderay'
+require 'lib/for_redcloth.rb'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -34,6 +36,7 @@ Rails::Initializer.run do |config|
   config.gem "populator"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
   config.gem "ruby-recaptcha"
+  config.gem "coderay"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
