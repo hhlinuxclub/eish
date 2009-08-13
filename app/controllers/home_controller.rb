78f80146584661acda353e837e00f6343672bd90
Joao Carlos <mail@joao-carlos.com>
2009-08-13 15:22:02 +0300

    Simplified controller by using Event.available. Added a respond_to block.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController  
   def index
     @news = News.find_all_by_published(true, :limit => 4, :order => "created_at DESC")
-    
-    ongoing_events = Event.ongoing
-    upcoming_events = Event.upcoming(4 - ongoing_events.count)
-    @events = ongoing_events + upcoming_events
-    
+    @events = Event.available(4)
     @user = current_user unless !logged_in?
     @welcome_message = Setting.option("welcome_message")
     @featured_article = Article.featured
+    
+    respond_to do |format|
+      format.html
+    end
   end
 end
