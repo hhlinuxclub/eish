c873a3a8c0f939e27da766eca830c55bd4452c6d
Joao Carlos <mail@joao-carlos.com>
2009-08-19 03:47:44 +0300

    Setting.option now returns different types if requested

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,6 +1,12 @@
 class Setting < ActiveRecord::Base
-  def self.option(option)
-    find_by_option(option).value
+  def self.option(option, type=:text)
+    include ActiveRecord::ConnectionAdapters
+    value = find_by_option(option).value
+    case type
+      when :integer   then value.to_i
+      when :boolean   then Column.value_to_boolean(value)
+      else value 
+    end
   end
   
   def self.featured_article_id
