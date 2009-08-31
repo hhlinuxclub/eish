d00dc90d9f8d7150b1d365c4e7432163ea3ea1bf
k33l0r <webadmin@draco-vulgaris.net>
2009-08-31 11:49:27 +0300

    Some cleanup [#167]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
   
   def index
-    @news = News.find_all_by_published(true, :limit => 4, :order => "published_at DESC")
-    @articles = Article.find_all_by_published(true, :limit => 4, :order => "published_at DESC")
+    news = News.find_all_by_published(true, :limit => 4, :select => "id, title, published_at", :order => "published_at DESC")
+    articles = Article.find_all_by_published(true, :limit => 4, :select => "id, title, published_at", :order => "published_at DESC")
     @events = Event.available(4)
     @user = current_user unless !logged_in?
     @welcome_message = Setting.option("welcome_message")
     @featured_article = Article.featured
     
-    @articles_and_news = [@news, @articles].flatten.sort { |x,y| y.published_at <=> x.published_at }
+    @articles_and_news = [news, articles].flatten.sort { |x,y| y.published_at <=> x.published_at }
     
     respond_to do |format|
       format.html
