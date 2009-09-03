12cfd7ddffb02505a385d5700c2603f6d0737f06
Joao Carlos <mail@joao-carlos.com>
2009-09-03 13:26:09 +0300

    Cleaner view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def same_time_distance?(collection, index)
+    time_ago_in_words(collection[index].published_at) != time_ago_in_words(collection[index-1].published_at) || index == 0
+  end
 end
