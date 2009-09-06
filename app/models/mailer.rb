228ffa4a0a619f71b004a3fd7ac15c4111e285ba
Joao Carlos <mail@joao-carlos.com>
2009-09-07 00:33:32 +0300

    Changed welcome emails to include an activation link [#101 state:testing]

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,25 +1,27 @@
 class Mailer < ActionMailer::Base
   def welcome(user)
     recipients    user.email
-    from          "HAAGA-HELIA Linux Club <do-not-reply@hhlinuxclub.fi>"
+    from          "HAAGA-HELIA Linux Club"
     subject       "Welcome to the HAAGA-HELIA Linux Club"
     sent_on       Time.now
     content_type  "multipart/alternative"
     
     part  "text/plain" do |p|
       p.body = render_message("welcome.plain.erb",
-      :user => user)
+      :user => user,
+      :host => "www.hhlinuxclub.org") #FIXME: Hardcoded host
       p.transfer_encoding = "base64"
     end
     
     part  :content_type => "text/html",
           :body => render_message("welcome.html.erb",
-          :user => user)
+          :user => user,
+          :host => "www.hhlinuxclub.org") # FIXME: Hardcoded host
   end
   
   def contact(recipient, user, contact_subject, message, ip_address, user_agent)
     recipients    recipient
-    from          "HHLC Contact Form <do-not-reply@hhlinuxclub.fi>"
+    from          "HHLC Contact Form"
     reply_to      user.email
     subject       contact_subject
     sent_on       Time.now
@@ -29,7 +31,7 @@ class Mailer < ActionMailer::Base
   
   def credentials(user, host)
     recipients    user.email
-    from          "HAAGA-HELIA Linux Club <do-not-reply@hhlinuxclub.fi>"
+    from          "HAAGA-HELIA Linux Club"
     subject       "Credentials request for the HAAGA-HELIA Linux Club"
     sent_on       Time.now
     content_type  "multipart/alternative"
